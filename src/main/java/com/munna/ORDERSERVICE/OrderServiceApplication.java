9bce8c9b491e9c7556ac05e31e134aea9f4f9ae7
puzah <kandimreddy@gmail.com>
2024-09-15 23:44:18 -0500

    code change for printing a message

--- src/main/java/com/munna/ORDERSERVICE/OrderServiceApplication.java
+++ src/main/java/com/munna/ORDERSERVICE/OrderServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hi!! I am Puzah.");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
